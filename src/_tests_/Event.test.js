c08f6f900dc72bac87f2ed19b887000f6482b446
MaryHoy <55214178+MaryHoy@users.noreply.github.com>
2020-03-05 14:38:12 -0800

    repair bug

--- src/_tests_/Event.test.js
+++ src/_tests_/Event.test.js
@@ -54,52 +54,52 @@ describe('<Event /> component', () => {
   test('set mock eventdata as state', () => {
     EventWrapper.setState({
       event: {
-        created: 1581545702000,
-        duration: 10800000,
-        id: "268665790",
-        name: "Olympia Home Service Professional Meetup 3",
+        created: 1563825339000,
+        duration: 9000000,
+        id: "263370107",
+        name: "Microservices mit dem MicroProfile 3.x",
         rsvp_limit: 80,
         date_in_series_pattern: false,
         status: "upcoming",
-        time: 1584666000000,
-        local_date: "2020-03-19",
-        local_time: "18:00",
-        updated: 1581631501000,
-        utc_offset: -25200000,
+        time: 1566925200000,
+        local_date: "2019-08-27",
+        local_time: "19:00",
+        updated: 1563825339000,
+        utc_offset: 7200000,
         waitlist_count: 79,
         yes_rsvp_count: 80,
         venue: {
-        id: 8832892,
-        name: "Waterstreet Cafe & Bar",
-        lat: 47.04268264770508,
-        lon: -122.90375518798828,
+        id: 26266792,
+        name: "adesso AG",
+        lat: 53.54524230957031,
+        lon: 9.950983047485352,
         repinned: false,
-        address_1: "610 Water Street Southwest",
-        city: "Olympia",
-        country: "us",
-        localized_country_name: "USA"
+        address_1: "Große Elbstraße 36",
+        city: "Hamburg",
+        country: "de",
+        localized_country_name: "Deutschland"
         },
         group: {
-        created: 1580510066000,
-        name: "Olympia Home Service Professional Mastermind Meetup",
-        id: 33279122,
+        created: 1387402147000,
+        name: "Java User Group Hamburg",
+        id: 11500362,
         join_mode: "open",
-        lat: 46.9900016784668,
-        lon: -122.87000274658203,
-        urlname: "Olympia-Small-Business-Meetup-Group",
-        who: "Members",
-        localized_location: "Olympia, WA",
-        state: "WA",
-        country: "us",
-        region: "en_US",
-        timezone: "US/Pacific"
+        lat: 53.54999923706055,
+        lon: 10,
+        urlname: "jug-hamburg",
+        who: "Mitglieder",
+        localized_location: "Hamburg, Deutschland",
+        state: "",
+        country: "de",
+        region: "de_DE",
+        timezone: "Europe/Berlin"
         },
-        link: "https://www.meetup.com/Olympia-Small-Business-Meetup-Group/events/268665790/",
-        description: "<p>THIS IS A FREE EVENT - PLEASE FINISH YOUR RSVP IN THE LINK BELOW</p> ",
+        link: "https://www.meetup.com/de-DE/jug-hamburg/events/263370107/",
+        description: "<p>Wer Microservices in Java entwickeln möchte, muss nicht zwangsläufig zu Spring Boot greifen. Es lohnt ein Blick auf alternative Frameworks, die in zunehmender Anzahl verfügbar sind und bezüglich ihrer Funktionalität stark aufholen. So schreitet auch die Entwicklung des MicroProfiles weiter voran. In beeindruckender Geschwindigkeit wurden zahlreiche APIs entwickelt, die (nicht nur) für die Entwicklung von Microservices sehr hilfreich sind.<br/>Hierzu zählen die Unterstützung von Metriken, Health Checks, Fault Tolerance und JSON Web Tokens. Für den Einsatz im Projekt kann aus unterschiedlichen Implementierungen wie TomEE, Thorntail oder OpenLiberty gewählt werden. In dieser Live-Coding-Session wird der praktische Einsatz von MicroProfile anhand eines Praxisbeispiels demonstriert.<br/> <br/> <br/>Referent:<br/>Thilo Frotscher arbeitet als freiberuflicher Softwarearchitekt und Trainer. Als Experte für Enterprise Java und Systemintegration unterstützt er seine Kunden überwiegend durch Entwicklung, Reviews oder die Durchführung von Schulungen. Thilo ist (Co-)Autor mehrerer Bücher in den Bereichen Java EE, (Web) Services und Systemintegration, hat zahlreiche Fachartikel verfasst und spricht regelmäßig auf Fachkonferenzen und Schulungsveranstaltungen oder bei Java User Groups.</p> ",
         visibility: "public",
         member_pay_fee: false
       }
     });
-    expect(EventWrapper.state('event').name).toBe('Olympia Home Service Professional Meetup 3');
+    expect(EventWrapper.state('event').name).toBe('Microservices mit dem MicroProfile 3.x');
   });
 });
