d4855da5cd3ea1bc5e392f4168b97f8326bff195
MaryHoy <55214178+MaryHoy@users.noreply.github.com>
2020-03-05 14:06:45 -0800

    4.4 updates

--- src/_tests_/CitySearch.test.js
+++ src/_tests_/CitySearch.test.js
@@ -3,37 +3,32 @@ import { shallow } from 'enzyme';
 import CitySearch from '../CitySearch';
 
 describe('<CitySearch /> component', () => {
-  
-    let CitySearchWrapper;
-    beforeAll(() => {
-      CitySearchWrapper = shallow(<CitySearch />);
-    });  
+
+  let CitySearchWrapper;
+  beforeAll(() => {
+    CitySearchWrapper = shallow(<CitySearch />);
+  });
 
   test('render text input', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find('.city')).toHaveLength(1);
   });
 
   test('render list of suggestions', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
   });
-  
+
   test('render text input correctly', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     const query = CitySearchWrapper.state('query');
     expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
   });
 
   test('change state when text input changes', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     const eventObject = { target: { value: 'Berlin' }};
     CitySearchWrapper.find('.city').simulate('change', eventObject);
     expect(CitySearchWrapper.state('query')).toBe('Berlin');
   });
 
   test('render list of suggestions correctly', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     const suggestions = CitySearchWrapper.state('suggestions');
     expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(suggestions.length);
     for (let i = 0; i < suggestions.length; i += 1) {
@@ -41,8 +36,8 @@ describe('<CitySearch /> component', () => {
     }
   });
 
-  test('click on suggestion should change query state', () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
+  test('click on suggestion should change query state and empty list of suggestions', () => {
+    CitySearchWrapper = shallow(<CitySearch updateEvents={() => {}}/>);
     CitySearchWrapper.setState({
       suggestions: [
         {
@@ -66,9 +61,39 @@ describe('<CitySearch /> component', () => {
         }
       ]
     });
-
+    expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(2);
     CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
     expect(CitySearchWrapper.state('query')).toBe('Munich, Germany');
+    expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(0);
   });
-  
+});
+
+describe('<CitySearch /> integration', () => {
+
+  test('get a list of cities when user searches for munich', async () => {
+    const CitySearchWrapper = shallow(<CitySearch />);
+    CitySearchWrapper.find('.city').simulate('change', { target: { value: 'Munich' } });
+    await CitySearchWrapper.update();
+    expect(CitySearchWrapper.state('suggestions')).toEqual([
+      {
+        city: 'Munich',
+        country: 'de',
+        localized_country_name: 'Germany',
+        name_string: 'Munich, Germany',
+        zip: 'meetup3',
+        lat: 48.14,
+        lon: 11.58
+      },
+      {
+        city: 'Munich',
+        country: 'us',
+        localized_country_name: 'USA',
+        state: 'ND',
+        name_string: 'Munich, North Dakota, USA',
+        zip: '58352',
+        lat: 48.66,
+        lon: -98.85
+      }
+    ]);
+  });
 });
---
+++ src/mock-events.js
@@ -0,0 +1,50 @@
+const mockEvents = {
+  city: {
+      id: 1007700,
+      city: 'München',
+      lat: 48.14,
+      lon: 11.58,
+      state: '',
+      country: 'de',
+      zip: 'meetup3',
+      member_count: 1257
+    },
+    events: [
+      {
+        created: 1582617054000,
+        duration: 5400000,
+        id: '268972894',
+        name: 'Live-Webinar: Der Design Your Life Kreislauf zur beruflichen Veränderung',
+        date_in_series_pattern: false,
+        status: 'upcoming',
+        time: 1583668800000,
+        local_date: '2020-03-08',
+        local_time: '13:00',
+        updated: 1582617163000,
+        utc_offset: 3600000,
+        waitlist_count: 0,
+        yes_rsvp_count: 22,
+        group: {
+          created: 1523352940000,
+          name: 'Design Your Life - Nachhaltig das eigene Leben gestalten',
+          id: 28110251,
+          join_mode: 'open',
+          lat: 48.139999389648438,
+          lon: 11.579999923706055,
+          urlname: 'Design-Your-Life-Nachhaltig-das-eigene-Leben-gestalten',
+          who: 'Mitglieder',
+          localized_location: 'München, Germany',
+          state: '',
+          country: 'de',
+          region: 'en_US',
+          timezone: 'Europe/Berlin'
+        },
+        link: 'https://www.meetup.com/Design-Your-Life-Nachhaltig-das-eigene-Leben-gestalten/events/268972894/',
+        description: '<p>registrieren um dann den EINWAHLLINK zu erhalten; also zwei Schritte um teilzunehmen.</p>  ',
+        visibility: 'public',
+        member_pay_fee: false
+    }
+  ]
+};
+
+export { mockEvents };
