50b8b335bdc7e67594f9ae0bc6a5ac4f474e549e
MaryHoy <55214178+MaryHoy@users.noreply.github.com>
2020-03-05 15:17:25 -0800

    update mock-events to test for error

--- src/mock-events.js
+++ src/mock-events.js
@@ -1,48 +1,60 @@
 const mockEvents = {
   city: {
-      id: 1007700,
-      city: 'München',
-      lat: 48.14,
-      lon: 11.58,
-      state: '',
-      country: 'de',
-      zip: 'meetup3',
-      member_count: 1257
-    },
-    events: [
-      {
-        created: 1582617054000,
-        duration: 5400000,
-        id: '268972894',
-        name: 'Live-Webinar: Der Design Your Life Kreislauf zur beruflichen Veränderung',
-        date_in_series_pattern: false,
-        status: 'upcoming',
-        time: 1583668800000,
-        local_date: '2020-03-08',
-        local_time: '13:00',
-        updated: 1582617163000,
-        utc_offset: 3600000,
-        waitlist_count: 0,
-        yes_rsvp_count: 22,
-        group: {
-          created: 1523352940000,
-          name: 'Design Your Life - Nachhaltig das eigene Leben gestalten',
-          id: 28110251,
-          join_mode: 'open',
-          lat: 48.139999389648438,
-          lon: 11.579999923706055,
-          urlname: 'Design-Your-Life-Nachhaltig-das-eigene-Leben-gestalten',
-          who: 'Mitglieder',
-          localized_location: 'München, Germany',
-          state: '',
-          country: 'de',
-          region: 'en_US',
-          timezone: 'Europe/Berlin'
-        },
-        link: 'https://www.meetup.com/Design-Your-Life-Nachhaltig-das-eigene-Leben-gestalten/events/268972894/',
-        description: '<p>registrieren um dann den EINWAHLLINK zu erhalten; also zwei Schritte um teilzunehmen.</p>  ',
-        visibility: 'public',
-        member_pay_fee: false
+    id: 1007700,
+    city: "München",
+    lat: 48.14,
+    lon: 11.58,
+    state: "",
+    country: "de",
+    zip: "meetup3",
+    member_count: 1257
+  },
+  events:[
+    {
+      created: 1563825339000,
+      duration: 9000000,
+      id: "263370107",
+      name: "Microservices mit dem MicroProfile 3.x",
+      rsvp_limit: 80,
+      date_in_series_pattern: false,
+      status: "upcoming",
+      time: 1566925200000,
+      local_date: "2019-08-27",
+      local_time: "19:00",
+      updated: 1563825339000,
+      utc_offset: 7200000,
+      waitlist_count: 79,
+      yes_rsvp_count: 80,
+      venue: {
+      id: 26266792,
+      name: "adesso AG",
+      lat: 53.54524230957031,
+      lon: 9.950983047485352,
+      repinned: false,
+      address_1: "Große Elbstraße 36",
+      city: "Hamburg",
+      country: "de",
+      localized_country_name: "Deutschland"
+      },
+      group: {
+      created: 1387402147000,
+      name: "Java User Group Hamburg",
+      id: 11500362,
+      join_mode: "open",
+      lat: 53.54999923706055,
+      lon: 10,
+      urlname: "jug-hamburg",
+      who: "Mitglieder",
+      localized_location: "Hamburg, Deutschland",
+      state: "",
+      country: "de",
+      region: "de_DE",
+      timezone: "Europe/Berlin"
+      },
+      link: "https://www.meetup.com/de-DE/jug-hamburg/events/263370107/",
+      description: "<p>Wer Microservices in Java entwickeln möchte, muss nicht zwangsläufig zu Spring Boot greifen. Es lohnt ein Blick auf alternative Frameworks, die in zunehmender Anzahl verfügbar sind und bezüglich ihrer Funktionalität stark aufholen. So schreitet auch die Entwicklung des MicroProfiles weiter voran. In beeindruckender Geschwindigkeit wurden zahlreiche APIs entwickelt, die (nicht nur) für die Entwicklung von Microservices sehr hilfreich sind.<br/>Hierzu zählen die Unterstützung von Metriken, Health Checks, Fault Tolerance und JSON Web Tokens. Für den Einsatz im Projekt kann aus unterschiedlichen Implementierungen wie TomEE, Thorntail oder OpenLiberty gewählt werden. In dieser Live-Coding-Session wird der praktische Einsatz von MicroProfile anhand eines Praxisbeispiels demonstriert.<br/> <br/> <br/>Referent:<br/>Thilo Frotscher arbeitet als freiberuflicher Softwarearchitekt und Trainer. Als Experte für Enterprise Java und Systemintegration unterstützt er seine Kunden überwiegend durch Entwicklung, Reviews oder die Durchführung von Schulungen. Thilo ist (Co-)Autor mehrerer Bücher in den Bereichen Java EE, (Web) Services und Systemintegration, hat zahlreiche Fachartikel verfasst und spricht regelmäßig auf Fachkonferenzen und Schulungsveranstaltungen oder bei Java User Groups.</p> ",
+      visibility: "public",
+      member_pay_fee: false
     }
   ]
 };
