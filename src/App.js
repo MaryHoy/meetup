bf1ebe354375cf62f897f4f6e1357e245050340f
MaryHoy <55214178+MaryHoy@users.noreply.github.com>
2020-03-18 16:49:49 -0700

    added offline alert

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents } from './api';
+import { OfflineAlert } from './Alert';
 
 class App extends Component {
 
   componentDidMount() {
     getEvents().then(response => this.setState({ events: response }));
+    window.addEventListener('online', this.offLineAlert());
   }
 
   state = {
@@ -17,7 +19,20 @@ class App extends Component {
     page: [],
     defaultCity: '',
     lat: null,
-    lon: null
+    lon: null,
+    OfflineText: ''
+  }
+
+  offLineAlert = () => {
+    if(navigator.onLine === false) {
+      this.setState({
+        offlineText: 'No internet connectiion. Please connect to view updated list.'
+      });
+    } else {
+      this.setState({
+        offlineText: '',
+      });
+    }
   }
 
   updateEvents = (lat, lon, page) => {
@@ -38,6 +53,7 @@ class App extends Component {
         <CitySearch updateEvents={this.updateEvents} />
         <NumberOfEvents updateEvents={this.updateEvents} numberOfEvents={this.state.events.length} lat={this.state.lat} lon={this.state.lon} />
         <EventList events={this.state.events} />
+        <OfflineAlert text={this.state.offlineText} />
       </div>
     );
   }
