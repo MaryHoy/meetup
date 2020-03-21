4b2b5dc386bb100850124e872dc5606c08125e7c
MaryHoy <55214178+MaryHoy@users.noreply.github.com>
2020-03-20 17:14:19 -0700

    added charts and graphs

--- src/Event.js
+++ src/Event.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { PieChart, Pie, Tooltip, Cell, Legend, ResponsiveContainer } from 'recharts';
 
 class Event extends Component {
   state = {
@@ -17,6 +18,9 @@ class Event extends Component {
 
   render() {
     const showDetails = this.state.showDetails;
+    const event = this.props.event;
+    const data=[{ name: "people coming", value: event.yes_rsvp_count }, { name: "open slots", value: (event.rsvp_limit - event.yes_rsvp_count) }];
+    const colors =[ "#8884d8", "#37c0ba"];
 
     return(
       <div className="event">
@@ -25,6 +29,22 @@ class Event extends Component {
           <p className="eventOverviewLocalDate">{this.props.event.local_date}</p>
           <button className="details-btn" onClick={() => this.handleShowDetails()}>Details</button>
         </div>
+        {event.rsvp_limit &&
+            <ResponsiveContainer height={150} width={250}>
+              <PieChart>
+              <Pie data= {data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={32}  label >
+              {
+                data.map((entry, index) => (<Cell key={`cell-${index}`} fill={colors[index]}/>))
+              }
+              </Pie>
+              <Legend iconSize={10} iconType = "triangle" layout="horizontal" verticalAlign="bottom" align="center" />
+              <Tooltip/>
+              </PieChart>
+            </ResponsiveContainer>
+          }
+          {!event.rsvp_limit &&
+            <p>{event.yes_rsvp_count} People Attending</p>
+          }
         {showDetails &&
           <div className="eventDetails">
             <p className="eventDetailsDescription">{this.props.event.description}</p>
